bd7f0d5a9725324afd3af60b7488968d9dda7ce5
dalescerri <ds32@hw.ac.uk>
2017-09-22 13:13:21 +0100

    Ramsey phase + Overhauser eigs -D
    Added basis angle rotation for spin readout.
    Plots P(Az) vs Az
    
    To do:
     - double check calulation of angle
     -send notes
     -check calculation of P(Az)

--- scripts/spin_bath/centralSpin_bath_v2.py
+++ scripts/spin_bath/centralSpin_bath_v2.py
@@ -3,6 +3,7 @@ import numpy as np
 import pylab as plt
 import math as mt
 import copy as cp
+import operator as op
 import scipy.linalg as lin
 import scipy.spatial.distance as dst
 import numpy.random as ran
@@ -199,9 +200,10 @@ class CentralSpinExperiment ():
 	def larm_vec (self):
 		lar_1 = np.zeros((len(self.Ap),3))
 		lar_0 = np.zeros((len(self.Ap),3))
+		#gam are in rad s-1 T-1, convert to Hz T-1
 		for f in range(len(self.Ap)):
-			lar_1[f] = self.gam_n*np.array([self.Bx,self.By,self.Bz])+np.array([self.Aox[f],self.Aoy[f],self.Ap[f]])
-			lar_0[f] = self.gam_n*np.array([self.Bx,self.By,self.Bz])
+			lar_1[f] = (self.gam_n/(2*np.pi))*np.array([self.Bx,self.By,self.Bz])+np.array([self.Aox[f],self.Aoy[f],self.Ap[f]])
+			lar_0[f] = (self.gam_n/(2*np.pi))*np.array([self.Bx,self.By,self.Bz])
 		return lar_0, lar_1
 
 	def _set_pars (self, tau):
@@ -333,6 +335,8 @@ class TimeEvol ():
 		# current density matrix for nuclear spin bath
 		self._curr_rho = []
 
+	def gaussian(self, x, mu, sig):
+		return 1./(sqrt(2.*pi)*sig)*np.exp(-np.power((x - mu)/sig, 2.)/2)
 
 	def set_experiment(self, nr_spins, concentration = .1):   
 		'''
@@ -352,17 +356,28 @@ class TimeEvol ():
 		self.exp.set_B_Cart (Bx=0, By=0 , Bz=1)
 		self.Larm = self.exp.larm_vec ()
 		self._nr_nucl_spins = self.exp._nr_nucl_spins
+		
+		#close_cntr = 0
+		#for j in range(self._nr_nucl_spins):
+		#	if np.sqrt(self.Aox[j]**2 + self.Aoy[j]**2 + self.Ap[j]**2) > 1e6:
+		#		self.Aox[j], self.Aoy[j], self.Ap[j] = 0, 0, 0
+		#	close_cntr +=1
+		#
+		#print '%d spins with |A| > 1MHz. Set Aoz, Aoy and Ap to 0.' %close_cntr
+
+		
 		#hyperfine vector
-		self.HFvec = np.array([[self.Aox[j], self.Aoy[j], self.Ap[j]] for j in range(self._nr_nucl_spins)]) 
+		self.HFvec = np.array([[self.Aox[j], self.Aoy[j], self.Ap[j]] for j in range(self._nr_nucl_spins)])
 
 		#Creating 2**N * 2**N spin Pauli matrices
 		self.In_tens = np.zeros((self._nr_nucl_spins,3,2**self._nr_nucl_spins,2**self._nr_nucl_spins),dtype=complex)
 		#initial bath density matrix
-		self._curr_rho = np.eye(2**self._nr_nucl_spins)/np.trace(np.eye(2**self._nr_nucl_spins))  
+		self._curr_rho = np.eye(2**self._nr_nucl_spins)/np.trace(np.eye(2**self._nr_nucl_spins))
 
-	def _op_var(self, Op):
+
+	def _op_sd(self, Op):
 		'''
-		Calculates the variance of operator Op with density matric rho
+		Calculates the standard deviation of operator Op with density matric rho
 
 		Input:
 		Op: matrix representing operator (whatever dimension)
@@ -396,11 +411,11 @@ class TimeEvol ():
 		for k in range(3): 
 			self._over_op[k] = sum(self.HFvec[j][k]*self.In_tens[j][k] for j in range(self._nr_nucl_spins))
 		return self._over_op
-
-	
-	def _U_op(self, ms, tau): 
+		
+		
+	def _H_op(self, ms):
 		'''
-		Returns matrix element <ms|U*rho*U|ms> 
+		Returns matrix element H_ms
 		(to be used to calculate the evolution in the Ramsey)
 
 		Input:
@@ -408,17 +423,37 @@ class TimeEvol ():
 		tau 	[seconds]	free-evolution time Ramsey
 		'''
 
-		Umsi = np.zeros((len(self.Ap),2,2),dtype=complex)
-		for g in range(len(self.Ap)):
+		Hmsi = np.zeros((self._nr_nucl_spins,2,2),dtype=complex)
+		for g in range(self._nr_nucl_spins):
+			Hmsi[g] = sum(self.Larm[ms][g][h]*self.In[h] for h in range(3))
+
+		Hms = Hmsi[0]
+		for g in range(1,self._nr_nucl_spins):
+			Hms = np.kron(Hms, Hmsi[g])
+
+		return Hms
+	
+	def _U_op(self, ms, tau): 
+		'''
+		Returns matrix element U_ms
+		(to be used to calculate the evolution in the Ramsey)
+
+		Input:
+		ms 		[0/1]		electron spin state
+		tau 	[seconds]	free-evolution time Ramsey
+		'''
+
+		Umsi = np.zeros((self._nr_nucl_spins,2,2),dtype=complex)
+		for g in range(self._nr_nucl_spins):
 			Umsi[g] = lin.expm(-complex(0,1)*sum(self.Larm[ms][g][h]*self.In[h] for h in range(3)) *tau)
 
 		Ums = Umsi[0]
-		for g in range(1,len(self.Ap)):
+		for g in range(1,self._nr_nucl_spins):
 			Ums = np.kron(Ums, Umsi[g])
 
 		return Ums
 	
-	def Ramsey (self, tau):
+	def Ramsey (self, phi, tau):
 		'''
 		Performs a single Ramsey experiment:
 		(1) calculates probabilities [p0, p1] to get ms=0,1
@@ -426,13 +461,14 @@ class TimeEvol ():
 		(3) updates the density matrix depending on the measurement outcome in (2)
 
 		Input: 
+		phi [radians]: Rotation angle of the spin readout basis
 		tau [seconds]: Ramsey sensing time
 
 		Output: outcome {0/1} of Ramsey experiment
 		'''
 
-		U0 = self._U_op(0, tau)-self._U_op(1, tau)
-		U1 = self._U_op(0, tau)+self._U_op(1, tau)
+		U0 = multiply(np.exp(-complex(0,1)*phi/2),self._U_op(0, tau)) - multiply(np.exp(complex(0,1)*phi/2),self._U_op(1, tau))
+		U1 = multiply(np.exp(-complex(0,1)*phi/2),self._U_op(0, tau)) + multiply(np.exp(complex(0,1)*phi/2),self._U_op(1, tau))
 		
 		#Ramsey result probabilities
 		p0 = .25*np.trace(U0.dot(self._curr_rho.dot(U0.conj().T))).real
@@ -441,22 +477,41 @@ class TimeEvol ():
 
 		ms = ran.choice([1,0],p=[p1, p0])
 		#evolution operator depending on Ramsey result:
-		U = self._U_op(0, tau)+((-1)**(ms+1))*self._U_op(1, tau)
+		U = multiply(np.exp(-complex(0,1)*phi/2),self._U_op(0, tau))+((-1)**(ms+1))*multiply(np.exp(complex(0,1)*phi/2),self._U_op(1, tau))
 	
 		rhoN_new = (U).dot(self._curr_rho.dot((U).conj().T))/ np.trace((U).dot(self._curr_rho.dot((U).conj().T)))
 		self._curr_rho = rhoN_new
 
 		return ms
-
-	def plot_field_distribution (self):
-
-		h = [self._curr_rho[i,i] for i in range(2**self._nr_nucl_spins)]
-
-		plt.figure(figsize = (12, 8))
-		plt.plot (h, 'o', color='royalblue')
-		plt.show()
-
-	def Nruns(self, nr_steps, tau_0, do_plot):
+	
+	def Prob_dens(self):
+		'''
+		(1) Calculates eigenvalues (Az) and (normalized) eigenvectors (|Az>) of the Overhauser Operator z component
+		(2) Sorts list of P(Az) = eigvec_prob[j] = Tr(|Az><Az| rho) according to sorted Az list (to plot later on)
+		(3) returns list of eigvals and P(Az) list
+		
+		Output:
+		eigvals       [kHz]: sorted Az list
+		eigvec_prob        : Tr(|Az><Az| rho) list sorted according to Az list
+		
+		Note:
+		I believe this is what's being shown in Fig.1 of DOI: 10.1103/PhysRevA.74.032316, although they start from a Gaussian distribution. Discuss.
+		'''
+		
+		eigvals, eigvecs = np.linalg.eig(self._overhauser_op()[2])
+		eigvecs = [x for (y,x) in sorted(zip(eigvals,eigvecs), key=lambda pair: pair[0])]
+		eigvals = multiply(1e-3, sorted(eigvals))
+		
+		#Calculate Tr(|Az><Az| rho)
+		eigvec_prob = np.zeros(2**self._nr_nucl_spins,dtype=complex)
+		for j in range(2**self._nr_nucl_spins):
+			eigvec_prob[j] = np.trace(self._curr_rho.dot(np.outer(np.conjugate(eigvecs[j]),eigvecs[j])))
+			
+		return eigvals, eigvec_prob
+	
+	
+	
+	def Nruns(self, nr_steps, phi_0, tau_0, do_plot):
 
 		'''
 		Runs full experimental sequence
@@ -468,59 +523,75 @@ class TimeEvol ():
 		'''
 
 		self.eigvals = []
+		self.eigvals_test = []
+		self.prob_dens_xaxis = []
 		self.Over_mean = [[] for j in range(3)]
-		self.Over_var = [[] for j in range(3)]
+		self.Over_sd = [[] for j in range(3)]
+		self.P_Az = []
 
 		for g in range(nr_steps+1):
 
-			self.tau = tau_0 #should be changed tau_k depending on adaptive protocol
+			self.tau = tau_0 #to be optimized with adaptive protocol
+			self.phi = phi_0 #to be optimized with adaptive protocol
 
 			if (g>0):
-				outcome = self.Ramsey(self.tau) 
+				outcome = self.Ramsey(self.phi, self.tau)
 				print 'Ramsey experiment  - outcome: ', outcome
 			
-			#self.plot_field_distribution()
-
 			for j in range(3):
 				self.Over_mean[j].append(self._op_mean(self._overhauser_op()[j]))
-				self.Over_var[j].append(self._op_var(self._overhauser_op()[j]))
+				self.Over_sd[j].append(self._op_sd(self._overhauser_op()[j]))
 			
-			self.eigvals.append(sorted(np.linalg.eig(self._curr_rho)[0], reverse=True))#sorted eigenvalues
-	
+			self.eigvals.append(sorted(np.linalg.eig(self._curr_rho)[0],reverse=True))#sorted eigenvalues
+			
+			self.P_Az.append(self.Prob_dens()[1])
+
+
 		if do_plot:
 		
 			ax_labels = ['x','y','z']
 
-			colormap = plt.get_cmap('hot')
+			colormap = plt.get_cmap('viridis')
 			color_list = [colormap(k) for k in np.linspace(0, 0.8, nr_steps+1)]
 			
-			#analytical values of intitial Overhauser Op components to double check
+			#analytical values of intitial s.d. of Overhauser components to double check
 			init_over_ticks = [.5* sum(self.HFvec[j][k]**2 for j in range(self._nr_nucl_spins))**.5 \
 								for k in range(3)]
 
-			fig = plt.figure(figsize = (15,6))
-			ax = fig.add_subplot(1, 1, 1)
+
+
+			#Plot P(Az) = Tr(|Az><Az| rho) against corresponding eigenvalues
+			fig1 = plt.figure(figsize = (15,6))
+			ax1 = fig1.add_subplot(1, 1, 1)
 			for j in range(nr_steps+1): 
-				ax.plot(range(2**self._nr_nucl_spins),self.eigvals[j], linewidth = 2, color=color_list[j])
+				ax1.plot(self.Prob_dens()[0],self.P_Az[j], linewidth = 2, color=color_list[j])
+			plt.xlabel (r'A$_z$', fontsize = 20)
+			plt.ylabel (r'P(A$_z$)', fontsize = 20)
+			plt.show()
+
+			fig2 = plt.figure(figsize = (15,6))
+			ax2 = fig2.add_subplot(1, 1, 1)
+			for j in range(nr_steps+1): 
+				ax2.plot(range(2**self._nr_nucl_spins),self.eigvals[j], linewidth = 2, color=color_list[j])
 			plt.xlabel ('nuclear spin number', fontsize = 20)
 			plt.show()
 			
-			fig2 = plt.figure(figsize = (15,6))
-			ax2 = fig2.add_subplot(1, 1, 1)
+			fig3 = plt.figure(figsize = (15,6))
+			ax3 = fig3.add_subplot(1, 1, 1)
 			for j in range(3):
-				ax2.plot(range(nr_steps+1),np.divide(self.Over_var[j],1000),
-						label=ax_labels[j]+'- component variance')
-			ax2.scatter(np.zeros(3), np.divide(init_over_ticks,1000), color='red',
-						label='analytical initial variance')
+				ax3.plot(range(nr_steps+1),np.divide(self.Over_sd[j],1000),
+						label=ax_labels[j]+'- component s.d.')
+			ax3.scatter(np.zeros(3), np.divide(init_over_ticks,1000), color='red',
+						label='analytical reciprocal of initial $T^*_2$')
 			plt.ylabel (r'$\sigma^{x,y,z}_{Over}$ (kHz)', fontsize=18)
 			plt.legend(fontsize=15)
 			plt.xlabel ('step nr', fontsize=18)
 
 			
-			fig3 = plt.figure(figsize = (15,6))
-			ax3 = fig3.add_subplot(1, 1, 1)
+			fig4 = plt.figure(figsize = (15,6))
+			ax4 = fig4.add_subplot(1, 1, 1)
 			for j in range(3):
-				ax3.plot(range(nr_steps+1),self.Over_mean[j],
+				ax4.plot(range(nr_steps+1),self.Over_mean[j],
 						label=ax_labels[j]+'- component mean')
 			plt.ylabel (r'$\mu^{x,y,z}_{Over}$ (Hz)', fontsize=18)
 			plt.xlabel ('step nr', fontsize=18)
@@ -534,4 +605,4 @@ class TimeEvol ():
 exp2 = TimeEvol()
 exp2.set_experiment(nr_spins=4)
 
-exp2.Nruns(nr_steps=5, tau_0=1e-6, do_plot=True)
+exp2.Nruns(nr_steps=10, phi_0 = 0, tau_0=1e-6, do_plot=True)
