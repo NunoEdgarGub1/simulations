d8cb4533b6b741ef194295e14ab381cb2f70d2d2
cbonato <c.bonato@hw.ac.uk>
2017-10-26 08:17:05 +0100

    teaching stuff -C

--- scripts/teaching/photonic_bandgaps.py
+++ scripts/teaching/photonic_bandgaps.py
@@ -56,8 +56,110 @@ def band_infinite (L1, L2, n1, n2):
 	plt.show()
 
 
-def reflectivityDBR ()
+def toy_bandgap (l0):
+	l = np.linspace (400, 1500, 2000)*1e-9
+	w = 2*np.pi*3e8/l
+	w0 = 2*np.pi*3e8/l0
+	phi = 0.5*np.pi*(l0/l)
+	n = 1.1
+
+	#A = 9*np.exp(2j*phi)-1
+	#B = 3-3*np.exp(-2j*phi)
+	#C = -3+3*np.exp(-2j*phi)
+	#D = 9*np.exp(-2j*phi)-1
+
+	A_D_2 = (n+1)**2*np.cos(2*phi) - (n-1)**2 # (A+D)/2 
+	Lambda = l0/4.
+
+	k = (1./Lambda)*np.arccos(A_D_2)
+
+	plt.figure (figsize=(10,10))
+	plt.plot (k*Lambda/np.pi, w/w0, linewidth=3, color = 'royalblue')
+	plt.plot (-k*Lambda/np.pi, w/w0, linewidth=3, color = 'royalblue')
+	#plt.axis ([-1,1, 1.45, 2.5])
+	plt.show()
+
+def DBR (N=5, th0 = 0., l0 = 900, n1=1.5, n2=2, do_plot = False):
+	
+	l_array = np.linspace (500, 1500, 1000)
+	R = np.zeros (len(l_array))
+
+	th1 = np.arcsin(np.sin(th0)/n1)
+	th2 = np.arcsin(n1*np.sin(th1)/n2)
+
+
+	D0 = np.matrix ([[1,1],[np.cos(th0), -np.cos(th0)]])
+	D1 = np.matrix ([[1,1],[n1*np.cos(th1), -n1*np.cos(th1)]])
+	D2 = np.matrix ([[1,1],[n2*np.cos(th2), -n2*np.cos(th2)]])
+
+	S01 = np.linalg.inv(D0)*D1
+	S12 = np.linalg.inv(D1)*D2
+	S20 = np.linalg.inv(D2)*D0
+	S21 = np.linalg.inv(D2)*D1
+
+	d1 = l0/(4*n1)
+	d2 = l0/(4*n2)
+
+	for ind,l in enumerate(l_array):
+		phi1 = n1*(2*np.pi/l)*d1
+		phi2 = n2*(2*np.pi/l)*d2
+
+		G1 = np.matrix([[np.exp(1j*phi1), 0],[0, np.exp(-1j*phi1)]])
+		G2 = np.matrix([[np.exp(1j*phi2), 0],[0, np.exp(-1j*phi2)]])
+
+		S = S20
+		for i in np.arange(N):
+			S = S*G2*S12*G1*S21
+
+		s21 = S[1,0]
+		s11 = S[0,0]
+
+		R[ind] = np.abs(s21/s11)**2
+
+	if do_plot:
+		plt.figure (figsize = (12, 6))
+		plt.plot (l_array, R, linewidth=3, color='royalblue')
+		plt.xlabel ('wavelength (nm)', fontsize = 20)
+		plt.ylabel ('reflectivity', fontsize = 20)
+		plt.show()
+
+	'''
+	plt.figure (figsize = (12, 6))
+	plt.plot (2*np.pi*3e8/l_array, R, linewidth=3, color='royalblue')
+	plt.xlabel ('frequency (Hz)', fontsize = 20)
+	plt.ylabel ('reflectivity', fontsize = 20)
+	plt.show()
+	'''
+
+	return l_array, R
+
+def compare_DBR_reflectivity (N = [3,5]):
+
+	plt.figure (figsize = (12, 6))
+
+	for n in N:
+		l, R = DBR (N=n)
+		plt.plot (l, R, linewidth=3)
+	plt.xlabel ('wavelength (nm)', fontsize = 20)
+	plt.ylabel ('reflectivity', fontsize = 20)
+	plt.show()
+
+def compare_DBR_stopband (n2 = [2, 2.5, 3]):
+
+	plt.figure (figsize = (12, 6))
+
+	for iii in n2:
+		l, R = DBR (N=10, n2=iii)
+		plt.plot (l, R, linewidth=3)
+	plt.xlabel ('wavelength (nm)', fontsize = 20)
+	plt.ylabel ('reflectivity', fontsize = 20)
+	plt.show()
 
 
 #reflectance (n1=1.5, n2=1.)
-band_infinite (L1= 170e-9, L2 = 100e-9, n1 = 3, n2 = 1.5)
+#band_infinite (L1= 170e-9, L2 = 100e-9, n1 = 3, n2 = 1.5)
+#toy_bandgap (l0=1000e-9)
+
+#DBR(N=10)
+compare_DBR_reflectivity (N = [3, 5, 10])
+compare_DBR_stopband()
