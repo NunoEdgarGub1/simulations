49a0e6b808027a64245c8b5dffeed7ac7ad0f0b9
cbonato <c.bonato@hw.ac.uk>
2019-05-08 14:10:22 +0100

    fixed something in dynamical decoupling with indep spins -CB

--- scripts/spin_bath/example_Nbath.py
+++ scripts/spin_bath/example_Nbath.py
@@ -18,6 +18,8 @@ exp.set_cluster_size(g=clus_size)
 
 # generates the bath with a given concentration of nuclear spins
 # (the total nr of spins was set before)
+B = 400 # field in gauss
+exp.set_magnetic_field (Bz=B*1e-4, Bx=0)
 exp.generate_bath (concentration = 0.011, name = 'bath1')
 # prints info about the bath
 #exp.print_nuclear_spins()
@@ -25,21 +27,17 @@ exp.generate_bath (concentration = 0.011, name = 'bath1')
 # sets thresholds
 # A: threshold for maximum hyperfine (to avoid strongly-coupled nuclear spins)
 exp.set_thresholds (A = 500e3, sparse = 10)
-B = 100 # field in gauss
-exp.set_magnetic_field (Bz=B*1e-4, Bx=0)
 #exp.FID_indep_Nspins (tau = np.linspace (0, 30e-6, 1000))
 
 # HAHN ECHO
 # we have two functions for Hahn echo, one that neglects all interactions betweeen 
 # nuclear spins (independent nuclear spins), the other uses the cluster approach
-tau_max= 5.e-3
-ind = exp.Hahn_echo_indep_Nspins (S1=1, S0=0, tau = np.linspace (0, tau_max, 100000), do_plot=False)
-nr_points_hahn = 100
-clus = exp.Hahn_echo (tau = np.linspace (0, tau_max, nr_points_hahn), phi = 0, do_plot = False)
+tau_max= .05e-3
+ind = exp.dynamical_decoupling_indep_Nspins (S1=1, S0=0, nr_pulses=8, tau= np.linspace (15e-6, tau_max, 10000))
+#clus = exp.Hahn_echo (tau = np.linspace (0, tau_max, nr_points_hahn), phi = 0, do_plot = False)
 
 plt.figure (figsize = (15,8))
-plt.plot (1e6*np.linspace (0, tau_max, 100000), ind, color = 'crimson')
-plt.plot (1e6*np.linspace (0, tau_max, nr_points_hahn), clus, color = 'royalblue')
+plt.plot (1e6*np.linspace (0, tau_max, 10000), ind, color = 'crimson')
 plt.xlabel ('tau (us)', fontsize = 15)
 plt.ylabel ('probability |0>', fontsize = 15)
 plt.show()
