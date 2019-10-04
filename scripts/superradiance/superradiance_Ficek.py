b282b8f5eefac4003f97c7544622e35d0eb54064
Ted S. Santana <sil.ted.s@gmail.com>
2019-10-04 20:27:47 -0300

    Superradiance according to Ficek's book (Quantum interference and coherence)

--- scripts/superradiance/superradiance_Ficek.py
+++ scripts/superradiance/superradiance_Ficek.py
@@ -69,7 +69,7 @@ class emitters(emitter):
 pl.ioff()
 hbar = const.hbar / const.elementary_charge * 1e15 # ueV.ns
 
-udir = pl.array([0,1])
+udir = pl.array([1,1j])
 u = 1.4*const.elementary_charge*udir/pl.norm(udir)  # dipole moment (x, y)
 wlen1 = 964.4                                       # resonance wavelength of the first emitter
 wlen2 = 964.4                                       # resonance wavelength of the second emitter
@@ -77,16 +77,17 @@ wlenl = 964.4                                       # wavelength of the laser
 w1 = 2*pl.pi*const.c/wlen1                          # angular frequency of the first emitter
 w2 = 2*pl.pi*const.c/wlen2                          # angular frequency of the second emitter
 wl = 2*pl.pi*const.c/wlenl                          # angular frequency of the laser
-r1 = -0.4*wlenl * pl.array([1, 0])                  # position of QD1 in nm
-r2 = +0.4*wlenl * pl.array([1, 0])                  # position of QD2 in nm
+r1 = -0.4*wlenl * pl.array([0,1])                  # position of QD1 in nm
+r2 = +0.4*wlenl * pl.array([0,1])                  # position of QD2 in nm
 Om1 = 0.28                                          # Rabi frequency in GHz
 Om2 = Om1                                           # Rabi frequency in GHz
-print("detuning = {:.3f}".format((w2-wl)*hbar))
+print("detuning = {:.3f} ueV".format((w2-wl)*hbar))
+print("distance = {:.1f} nm".format(pl.norm(r2-r1)))
 
 QDs = emitters(wlen1, wlen2, u, r1, r2)
 #QDs.QD1.G = 1.6
 #QDs.QD2.G = 1.8
-rates = pl.zeros((2,2))
+rates = pl.zeros((2,2), dtype = pl.complex_)
 rates[0,0] = QDs.G(1,1); rates[0,1] = QDs.G(1,2);
 rates[1,0] = QDs.G(2,1); rates[1,1] = QDs.G(2,2);
 print(rates)
@@ -103,16 +104,15 @@ sigM2 = tensor(pl.eye(2), sigM)
 sigP1 = adj(sigM1)
 sigP2 = adj(sigM2)
 
-# For real dipole moment
-Hs = QDs.Lambda()*(pl.dot(sigP1, sigM2)+pl.dot(sigP2, sigM1))
-Hl = 0.5*(Om1*sigP1 + Om2*sigP2 + Om1*sigM1 + Om2*sigM2)
+Hs = QDs.Lambda()*pl.dot(sigP1, sigM2)+pl.conj(QDs.Lambda())*pl.dot(sigP2, sigM1)
+Hl = -0.5j*(Om1*sigP1 + Om2*sigP2) + 0.5j*(pl.conj(Om1)*sigM1 + pl.conj(Om2)*sigM2)
 H = Hs + Hl
 H[1,1] = (w2-wl); H[3,3] = (w2-wl)
 
 ################# Master equation #################
 yi = pl.zeros((4,4), dtype = pl.complex_)
 yi[0,0] = 1
-sys = masterEquation(yi, H, sigM1, sigM2, n=1e3, dt=1e-2, rates = rates)
+sys = masterEquation(yi, H, sigM1, sigM2, n=2e3, dt=1e-2, rates = rates)
 #sys.get_steady_state()
 #sys.trajectory(atol = 1e-9, rtol=1e-9)
 sys.g2Func(sigM1, sigM2, atol = 1e-9, rtol=1e-9)
@@ -129,14 +129,14 @@ pl.tick_params('both', length=15, width=2, which='major', labelsize=26, directio
 pl.tick_params('both', length=7, width=1, which='minor', direction='in', top=True, right=True)
 pl.title("g2-superradiance - Ficek", fontsize=16)
 pl.plot(sys.time, sys.g2, 'r-', lw = 2)
-pl.gca().xaxis.set_major_locator(pl.MultipleLocator(5))
-pl.gca().xaxis.set_minor_locator(pl.MultipleLocator(2.5))
+pl.gca().xaxis.set_major_locator(pl.MultipleLocator(10))
+pl.gca().xaxis.set_minor_locator(pl.MultipleLocator(5))
 pl.gca().yaxis.set_major_locator(pl.MultipleLocator(0.5))
 pl.gca().yaxis.set_minor_locator(pl.MultipleLocator(0.25))
 pl.xlabel('$\\tau$ (ns)', fontsize = 26, labelpad=0)
 pl.ylabel('$g^{(2)}(\\tau)$', fontsize = 26, labelpad=0)
 pl.xlim(min(sys.time), max(sys.time))
-pl.ylim(-0.1, 1.3)
+#pl.ylim(-0.1, 1.1)
 pl.hlines(0, min(sys.time), max(sys.time), colors = 'k', linestyles = '--')
 pl.hlines(0.5, min(sys.time), max(sys.time), colors = 'k', linestyles = '--')
 pl.hlines(1, min(sys.time), max(sys.time), colors = 'k', linestyles = '--')
